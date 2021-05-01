c92d405fc1e036169f8d131bae6a96f01c049d22
Daniel Hooper <danielhhooper@gmail.com>
2021-05-01 15:33:34 -0400

    Initial Commit

---
+++ Neotek Remote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neotek Remote
+//
+//  Created by Daniel Hooper on 2021-05-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Neotek Remote/Neotek_RemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neotek_RemoteApp.swift
+//  Neotek Remote
+//
+//  Created by Daniel Hooper on 2021-05-01.
+//
+
+import SwiftUI
+
+@main
+struct Neotek_RemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
